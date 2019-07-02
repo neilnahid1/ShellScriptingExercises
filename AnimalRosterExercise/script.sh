b42ca3d3010c903a61b3786316d027c5c512d9c9
Neil Francis Nahid <neil.nahid@fullspeedtechnologies.com>
2019-07-02 18:05:53 +0800

    did somebugfixing and added an argument that which outputs according to the category

--- AnimalRosterExercise/script.sh
+++ AnimalRosterExercise/script.sh
@@ -7,12 +7,19 @@ do
     counter=0
     for word in $line; #iterate through each word of a line separated by comma
     do
-        if [ $counter -eq 1 ]; #get the type on the second index word of the line
+        if [ $counter -eq 0 ]; #get the category which is the first index word on the line.
+        then
+            category="$word"
+        fi
+        if [ $counter -eq 1 ]; #get the type on the second indexed word on the line
         then
             type="$word"
         fi
         counter=` expr $counter + 1 `
     done
+    if [ $category == "$1" ]
+    then
     counter=` expr $counter - 2 ` #subtract by two since the first 2 words are for category and types
     echo "$type count: $counter"
+    fi
 done
